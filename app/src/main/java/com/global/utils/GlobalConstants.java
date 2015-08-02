c637353072ac7bf23f06575a22069c9f8f792ccf
hobbes09 <sourin.sutradhar@gmail.com>
2015-08-02 09:01:20 +0530

    path corrections

--- app/src/main/java/com/global/utils/GlobalConstants.java
+++ app/src/main/java/com/global/utils/GlobalConstants.java
@@ -6,4 +6,5 @@ package com.global.utils;
 public class GlobalConstants {
 
     public static final int SPLASH_SCREEN_TIMEOUT = 3000;
+    public static int NUM_ADS = 5;  // Needs to be fixed at the time of loading in SplashScreen
 }
