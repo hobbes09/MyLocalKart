ffd0b4933db390137de3c592820be190fbed626c
hobbes09 <sourin.sutradhar@gmail.com>
2015-09-19 04:57:51 +0530

    Fixing number of columns for subcategory

--- app/src/main/java/com/global/utils/GlobalConstants.java
+++ app/src/main/java/com/global/utils/GlobalConstants.java
@@ -11,6 +11,7 @@ public class GlobalConstants {
 
     public static final int SPLASH_SCREEN_TIMEOUT = 3000;
     public static int NUM_ADS = 5;  // Needs to be fixed at the time of loading in SplashScreen
+    public static int NUM_COLUMNS_SUBCATEGORY = 2;
 
     public static String mCurrentUser;
     public static String mCurrentUserEmail;
